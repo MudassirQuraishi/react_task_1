3557c8fe58e116a3130bd9e3655efadb70028aae
MudassirQuraishi <mudassir.quraishi14@outlook.com>
2023-12-23 20:26:59 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Heading from "./components/Heading";
+import List from "./components/List";
+
+function App() {
+    return (
+        <div>
+            <Heading></Heading>
+            <List></List>
+        </div>
+    );
+}
+
+export default App;
