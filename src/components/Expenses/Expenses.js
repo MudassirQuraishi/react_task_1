9289d67a02b6ed7347460c70208675b3d6095321
MudassirQuraishi <mudassir.quraishi14@outlook.com>
2023-12-27 18:30:33 +0530

    Assignment-2 task-11

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -11,6 +11,9 @@ const Expenses = (props) => {
     const filterChangeHandler = (selectedYear) => {
         setFilteredYear(selectedYear);
     };
+    const filteredExpenses = props.items.filter(
+        (expense) => expense.date.getFullYear() === parseInt(filteredYear)
+    );
 
     return (
         <div>
@@ -19,7 +22,7 @@ const Expenses = (props) => {
                     selected={filteredYear}
                     onChangeFilter={filterChangeHandler}
                 />
-                {props.items.map((expense) => (
+                {filteredExpenses.map((expense) => (
                     <ExpenseItem
                         key={expense.id}
                         title={expense.title}
