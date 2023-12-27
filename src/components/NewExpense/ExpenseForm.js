8140a6508cc2a693c0d0272907ff6bb8f4dd74c3
MudassirQuraishi <mudassir.quraishi14@outlook.com>
2023-12-27 20:41:05 +0530

    Assignment-2 task-13

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -6,6 +6,7 @@ const ExpenseForm = (props) => {
     const [enteredTitle, setEnteredTitle] = useState("");
     const [enteredAmount, setEnteredAmount] = useState("");
     const [enteredDate, setEnteredDate] = useState("");
+    const [showForm, setShowFormStatus] = useState(false);
 
     const titleChangeHandler = (event) => {
         setEnteredTitle(event.target.value);
@@ -32,44 +33,66 @@ const ExpenseForm = (props) => {
         setEnteredAmount("");
         setEnteredDate("");
     };
-
-    return (
-        <form onSubmit={submitHandler}>
-            <div className='new-expense__controls'>
-                <div className='new-expense__control'>
-                    <label>Title</label>
-                    <input
-                        type='text'
-                        value={enteredTitle}
-                        onChange={titleChangeHandler}
-                    />
-                </div>
-                <div className='new-expense__control'>
-                    <label>Amount</label>
-                    <input
-                        type='number'
-                        min='0.01'
-                        step='0.01'
-                        value={enteredAmount}
-                        onChange={amountChangeHandler}
-                    />
-                </div>
-                <div className='new-expense__control'>
-                    <label>Date</label>
-                    <input
-                        type='date'
-                        min='2019-01-01'
-                        max='2022-12-31'
-                        value={enteredDate}
-                        onChange={dateChangeHandler}
-                    />
-                </div>
-            </div>
-            <div className='new-expense__actions'>
-                <button type='submit'>Add Expense</button>
-            </div>
-        </form>
+    const hideFormHandler = (e) => {
+        setShowFormStatus(false);
+    };
+    const showFormHandler = (e) => {
+        setShowFormStatus(true);
+    };
+    let formContent = (
+        <button type='submit' onClick={showFormHandler}>
+            Add Expense
+        </button>
     );
+    if (showForm === true) {
+        formContent = (
+            <>
+                <div className='new-expense__controls'>
+                    <div className='new-expense__control'>
+                        <label>Title</label>
+                        <input
+                            type='text'
+                            value={enteredTitle}
+                            onChange={titleChangeHandler}
+                        />
+                    </div>
+                    <div className='new-expense__control'>
+                        <label>Amount</label>
+                        <input
+                            type='number'
+                            min='0.01'
+                            step='0.01'
+                            value={enteredAmount}
+                            onChange={amountChangeHandler}
+                        />
+                    </div>
+                    <div className='new-expense__control'>
+                        <label>Date</label>
+                        <input
+                            type='date'
+                            min='2019-01-01'
+                            value={enteredDate}
+                            onChange={dateChangeHandler}
+                        />
+                    </div>
+                </div>
+                <div className='new-expense__actions'>
+                    <button type='reset' on onClick={hideFormHandler}>
+                        Cancel
+                    </button>
+                    <button type='submit'>Add Expense</button>
+                </div>
+            </>
+        );
+    } else if (showForm === false) {
+        formContent = (
+            <button type='submit' onClick={showFormHandler}>
+                Add Expense
+            </button>
+        );
+    }
+
+    return <form onSubmit={submitHandler}>{formContent}</form>;
 };
 
 export default ExpenseForm;
