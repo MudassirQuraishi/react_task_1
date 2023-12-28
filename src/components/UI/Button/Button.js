7e404e28914504e9013c093175383e26fce2f89c
MudassirQuraishi <mudassir.quraishi14@outlook.com>
2023-12-28 14:31:03 +0530

    Assignment-3 task-1

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React from "react";
 
-import './Button.css';
+import "./Button.css";
 
-const Button = props => {
-  return (
-    <button type={props.type} className="button" onClick={props.onClick}>
-      {props.children}
-    </button>
-  );
+const Button = (props) => {
+    return (
+        <button
+            type={props.type}
+            className={`button ${!props.inValid ? "invalid" : ""}`}
+            onClick={props.onClick}>
+            {props.children}
+        </button>
+    );
 };
 
 export default Button;
